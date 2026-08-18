7ce43c3782d91b0539c81178659cf9cae748d933
“Omar <omargomaa9102006@gmail.com>
2026-08-19 01:43:20 +0300

    odom storing position

--- first_pkg/first_pkg/movementNode.py
+++ first_pkg/first_pkg/movementNode.py
@@ -2,13 +2,17 @@
 import rclpy 
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+import time
 
 
 class actionServer(Node):
     def __init__(self):
         super().__init__("actionServer")
         self.publisher_ = self.create_publisher(Twist,'/cmd_vel' ,10)
-        #subscribe to odom
+        self.subscrib_odom = self.create_subscription(Odometry,'/odom',self.current_pose,10)
+        self.Pose = None
+        self.current_yaw = 0.0
         self.get_logger().info("started")
 
 
@@ -20,12 +24,24 @@ class actionServer(Node):
         #implement action not just a function
         pass
 
+    def current_pose(self,msg:Odometry):
+        self.Pose = (msg.pose.pose.position.x , msg.pose.pose.position.y )
+        self.current_yaw = (msg.pose.pose.orientation.z,msg.pose.pose.orientation.w)
+        time.sleep(1)
+        output = f"""
+                     Pos:{self.Pose}
+                     orientation:{self.current_yaw}
+                     Linear :{(msg.twist.twist.linear.x,msg.twist.twist.linear.y)}
+                     angular: {(msg.twist.twist.angular.z)}"""
+                     
+        self.get_logger().info(output)
+        pass
 
 
 def main(args = None):
     rclpy.init(args=args)
-    #driver code idk
     node = actionServer()
+    rclpy.spin(node)
     rclpy.shutdown()    
 
 if __name__ == "__main__":
--- first_pkg/first_pkg/node1.py
+++ first_pkg/first_pkg/node1.py
@@ -2,18 +2,26 @@
 import rclpy 
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 import time
 
 class MyNode(Node):
     def __init__(self):
         super().__init__("first_node")
         self.publisher_ = self.create_publisher(Twist,'/cmd_vel' ,10)
-        self.direction = 1.0
+        self.publisher_c = self.create_publisher( Bool,'/finish_robot/touched',10)
+        m = Bool()
+        m.data = True
+        self.publisher_c.publish(m)
+        self.direction = -1.0
         self.get_logger().info("started")
     def move(self):
         message = Twist()
-        message.linear.z = self.direction * 3
-        message.linear.x = self.direction * 3
+        message.linear.x = 0.0 
+        message.linear.y = 0.0
+        message.angular.z = 1.0
+        message.angular.x = 0.0
+        message.angular.y = 0.0
         self.direction *= -1
         self.publisher_.publish(message)
 
@@ -24,7 +32,7 @@ def main(args = None):
     node = MyNode()
     while True:
         node.move()
-        time.sleep(0.8)
+        time.sleep(0.5)
 
     rclpy.shutdown()    
 
